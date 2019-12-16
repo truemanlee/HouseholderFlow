e7d6602ee29e2f4ad9baf0fb64911b9af98f13f8
ChuminLi <lichm5@mail2.sysu.edu.cn>
2019-12-16 18:08:00 +0800

    Create HouseholderFlow.py

---
+++ HouseholderFlow.py
@@ -0,0 +1,56 @@
+import numpy as np
+import tensorflow as tf 
+
+# HouseHolder变换
+# z' = (I - 2vv^T/||vt||^2) z
+# z' = H z
+class HouseHolderFlow():
+    def __init__(self, num_flow):
+        self.num_flow = num_flow
+
+    def hf_transform(self, z, mu, layer_index):
+        with tf.variable_scope('hf_transform_{}'.format(layer_index)):
+            # z:[B, D] -> [B, D]
+            dims = z.shape().as_list()[-1]
+            batch_size = tf.shape(z)[0]
+            I = tf.eye(dims) # [D, D]
+            v = tf.get_variable('v_{}'.format(i), [dims], tf.float32)
+            v_norm = tf.reduce_sum(v ** 2) # 可能需要防溢出措施
+            v = tf.expand_dims(v, axis=-1) # [D, 1]
+            v = tf.matmul(v, v, transpose_b=True) # [D, D]
+            H = I - 2 * v / v_norm
+            z_t = tf.transpose(tf.matmul(H, z, transpose_b=True), [1, 0])
+            mu_t = tf.transpose(tf.matmul(H, mu, transpose_b=True), [1, 0])
+            return z_t, mu_t
+
+    def NN_encoder(self, inputs):
+        # mu and sigma encoder
+        mean = inputs
+        stddev = tf.nn.softplus(inputs)
+        return mean, stddev
+
+    def hf_flow(self, inputs):
+        output = dict()
+        mu, sigma = self.NN_encoder(inputs) # [B, D], [B, D]
+        z = mu + sigma * tf.random_normal(tf.shape(mu), 0, 1, dtype=tf.float32)
+        output['mu'] = mu
+        output['sigma'] = sigma
+        output['z'] = z
+        for i in range(self.num_flow):
+            z, mu_t = self.hf_transform(z, i)
+        output['z_t'] = z
+        output['mu_t'] = mu_t
+        return output
+
+    def NN_decoder(self, inputs):
+        outputs = inputs
+        return outputs
+
+    def add_loss(self, hf_output, targets):
+        # reconstruction error & KL loss
+        rec = self.NN_decoder(hf_output['zt'])
+        loss1 = tf.reduce_mean((ref - targets) ** 2)
+        # KL loss
+        # householder flow does not change the stddev but changes the mu, and mu = H_t H_{t-1} ... H1
+        loss2 = 0.5 * (-tf.log(hf_output['sigma']) - dims + hf_output['sigma'] + (hf_output['mu'] - hf_output['zmu']))
+        return loss1 - loss2
